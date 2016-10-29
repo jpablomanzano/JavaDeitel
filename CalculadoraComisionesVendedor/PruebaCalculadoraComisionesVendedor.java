acd2e22e4d65c7c7b7afa465962f04018dd6d896
jpablomanzano <juanpi9_@hotmail.com>
2016-10-29 01:42:56 -0500

    Create main in PruebaCalculadoraComisionesVendedor

--- CalculadoraComisionesVendedor/PruebaCalculadoraComisionesVendedor.java
+++ CalculadoraComisionesVendedor/PruebaCalculadoraComisionesVendedor.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class PruebaCalculadoraComisionesVendedor{
+
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+
+  }
 
 }
