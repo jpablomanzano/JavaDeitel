28ef45b7cb2289156baa5b7b9cf21a6fce1e742a
jpablomanzano <juanpi9_@hotmail.com>
2016-10-28 22:15:37 -0500

    Actualizando variables

--- CalculadoraComisionesVendedor/ComisionesVendedor.java
+++ CalculadoraComisionesVendedor/ComisionesVendedor.java
@@ -2,9 +2,9 @@ public class ComisionesVendedor{
 
   private String nombreVendedor;
   private double sueldoBaseVendedor;
+  private double porcentajeComisionVendedor;
   private double comisionesVendedorMes;
   private double totalSueldoVendedor;
-  private int articulo;
-
+  private double precioArticulo;
 
 }
