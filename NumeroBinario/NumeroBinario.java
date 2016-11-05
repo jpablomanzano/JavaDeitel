d0569510a2544132b132432a23dbf24a2a073a75
jpablomanzano <juanpi9_@hotmail.com>
2016-11-05 12:31:51 -0600

    Agrego comentarios a NumeroBinario.java

--- NumeroBinario/NumeroBinario.java
+++ NumeroBinario/NumeroBinario.java
@@ -1,10 +1,10 @@
 //Programa que convierte un número binario a decimal
-
+//Importo Scanner
 import java.util.Scanner;
 
 public class NumeroBinario{
 
-  public static void main(String[] args) {
+  public static void main(String[] args) {//Inicio main
 
     Scanner entrada = new Scanner(System.in);
 
@@ -14,25 +14,33 @@ public class NumeroBinario{
     int modulo = 10; // El modulo del residuo para obtener el numeroConvertir
     int posicionBinaria = 1; //Por lo que se va a multiplicar numeroConvertir
     int suma = 0;
-    int valor;
 
-
+    //Solicito a mi usuario el número binario y lo guardo en la variable original
     System.out.print("Ingrese el número binario: ");
     numero = entrada.nextInt();
 
+    //Creo mi copia de la variable original
     residuo = numero;
+
+    //comienzo mi ciclo while
     while(residuo != 0){
 
+      //Obtengo mi uno o cero que está a la derecha
       numeroConvertir = residuo % modulo;
+
+      // Divido a mi copia de número original entre 10 para correr un lugar a la derecha
+      // Al ser de tipo Int los números a la izquierda se eliminan
       residuo = residuo / modulo;
 
-      valor = numeroConvertir * posicionBinaria;
-      suma = suma + valor;
+      // Acumulo en suma mi numeroConvertir por la posicionBinaria que le corresponde
+      suma = suma + (numeroConvertir * posicionBinaria);
 
+      //Multiplico en 2 posicionBinaria para obtener cuanto se multiplicará el siguiente número
       posicionBinaria = posicionBinaria * 2;
 
     }//Fin while
 
+    //Imprimo mi resultado
     System.out.println("Valor decimal: " + suma);
 
   }//Fin main
