1f23df25dabf02296569240de90692e99517185b
Juan Pablo Mazano <juanpi9_@hotmail.com>
2016-11-05 21:58:36 -0600

    Create Factorial.java

---
+++ Factoriales/Factorial.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pruebas {
+
+    
+    public static void main(String[] args) {
+        
+        int numero;
+        int factorial = 1;
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.print("Ingrese el número: ");
+        numero = input.nextInt();
+        
+        while(numero < 0){
+            System.out.print("Número inválido, ingrese enteros positivos. Ingrese número ");
+            numero = input.nextInt();
+        }
+        
+        if (numero == 0 || numero == 1) {
+            System.out.println("El factorial es:" + factorial);
+        }else{
+            factorial = factorial * numero;
+            while (numero > 1) {                
+                factorial = factorial * (numero-1);
+                --numero;
+            }
+            System.out.println(factorial);
+        }
+    }
+    
+}
