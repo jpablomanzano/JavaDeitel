83b4b54d90ed202b089c017e7716ce368697a05f
Juan Pablo Manzano <juanpi9_@hotmail.com>
2016-11-08 08:39:23 -0600

    Update DesencriptarNumero.java

--- EncriptarNumero/DesencriptarNumero.java
+++ EncriptarNumero/DesencriptarNumero.java
@@ -1,70 +1,81 @@
 public class DesencriptarNumero
 {
   private int numeroEncriptado;
-  private String numero;
+    private String numero;
 
-  public DesencriptarNumero(int numero)
-  {
-    numeroEncriptado = numero;
-   }
+    public DesencriptarNumero(int numero)
+    {
+        numeroEncriptado = numero;
+    }
 
-  public void establecerNumeroEncriptado(int numero)
-  {
-    numeroEncriptado = numero;
-  }
+    public void establecerNumeroEncriptado(int numero)
+    {
+        numeroEncriptado = numero;
+    }
 
-  public int obtenerNumeroEncriptado()
-  {
-    return numeroEncriptado;
-  }
+    public int obtenerNumeroEncriptado()
+    {
+        return numeroEncriptado;
+    }
 
-  public void establecerNumero()
-  {
-    int numero1;
-    int numero2;
-    int numero3;
-    int numero4;
+    public void establecerNumero()
+    {
+        int numero1;
+        int numero2;
+        int numero3;
+        int numero4;
 
-    int cambio1;
-    int cambio2;
-
-    numero4 = numeroEncriptado % 10;
-    numeroEncriptado = numeroEncriptado / 10;
-
-    numero3 = numeroEncriptado % 10;
-    numeroEncriptado = numeroEncriptado / 10;
-
-    numero2 = numeroEncriptado % 10;
-    numeroEncriptado = numeroEncriptado / 10;
-
-    numero1 = numeroEncriptado % 10;
-    numeroEncriptado = numeroEncriptado / 10;
+        int cambio1;
+        int cambio2;
 
 
-    cambio1 = numero1;
-    numero1 = numero3;
-    numero3 = cambio1;
+        //Separo dígitos de DERECHA a IZQUIERDA
+        numero4 = numeroEncriptado % 10;
+        numeroEncriptado = numeroEncriptado / 10;
 
-    cambio2 = numero2;
-    numero2 = numero4;
-    numero4 = cambio1;
+        numero3 = numeroEncriptado % 10;
+        numeroEncriptado = numeroEncriptado / 10;
 
+        numero2 = numeroEncriptado % 10;
+        numeroEncriptado = numeroEncriptado / 10;
 
-    numero4 = numero4 + 10;
-    numero3 = numero3 + 10;
-    numero2 = numero2 + 10;
-    numero1 = numero1 + 10;
+        numero1 = numeroEncriptado % 10;
+        numeroEncriptado = numeroEncriptado / 10;
 
-    numero4 = numero4 - 7;
-    numero3 = numero3 - 7;
-    numero2 = numero2 - 7;
-    numero1 = numero1 - 7;
+        //Camio numero1 con numero3 y viceversa
+        cambio1 = numero1;
+        numero1 = numero3;
+        numero3 = cambio1;
 
-    numero = Integer.toString(numero1) + Integer.toString(numero2) + Integer.toString(numero3) + Integer.toString(numero4);
+        //Camio numero2 con numero4 y viceversa
+        cambio2 = numero2;
+        numero2 = numero4;
+        numero4 = cambio2;
 
-  }
+        //Sumo 10 a cada variable
+        numero4 = numero4 + 10;
+        numero3 = numero3 + 10;
+        numero2 = numero2 + 10;
+        numero1 = numero1 + 10;
 
-  public String obtenerNumero(){
-    return numero;
-  }
+        //Resto 7 a cada variable
+        numero4 = numero4 - 7;
+        numero3 = numero3 - 7;
+        numero2 = numero2 - 7;
+        numero1 = numero1 - 7;
+
+        //Asigno a cada variable el módulo de 10 de si mismo
+        numero4 = numero4 % 10;
+        numero3 = numero3 % 10;
+        numero2 = numero2 % 10;
+        numero1 = numero1 % 10;
+
+        //Asigno el valor String de cada variable concatenada
+        numero = Integer.toString(numero1) + Integer.toString(numero2) + Integer.toString(numero3) + Integer.toString(numero4);
+
+    }
+
+    public String obtenerNumero(){
+        return numero;
+    }
 }
