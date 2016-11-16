38db54d357a3b3be05c1ec637eb52172815012e7
jpablomanzano <juanpi9_@hotmail.com>
2016-11-16 08:05:15 -0600

    ACtualizado

--- Factoriales/Factorial.java
+++ Factoriales/Factorial.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 public class Factorial {
 
-    
+
     public static void main(String[] args) {
-        
+
         int numero;
         int factorial = 1;
-        
+
         Scanner input = new Scanner(System.in);
-        
-        
+
+
         System.out.print("Ingrese el número: ");
         numero = input.nextInt();
-        
+
         while(numero < 0){
             System.out.print("Número inválido, ingrese enteros positivos. Ingrese número ");
             numero = input.nextInt();
         }
-        
+
         if (numero == 0 || numero == 1) {
             System.out.println("El factorial es:" + factorial);
         }else{
             factorial = factorial * numero;
-            while (numero > 1) {                
+            while (numero > 1) {
                 factorial = factorial * (numero-1);
                 --numero;
             }
             System.out.println("El factorial es: "+ factorial);
         }
     }
-    
+
 }
---
+++ Intereses/Intereses.java
@@ -0,0 +1,12 @@
+
+public class Intereses
+{
+  public static void main(String[] args) {
+    double monto;
+    double principal = 100.0;
+    double tasa = 0.05;
+
+    //Encabezados
+    System.out.printf()
+  }
+}
