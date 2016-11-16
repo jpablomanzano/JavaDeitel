d7874a632d098af1c650a6567abaf9101584afbf
Juan Pablo Manzano <juanpi9_@hotmail.com>
2016-11-16 15:48:31 -0600

    Actualizando clase Intereses
    Se culmina la clase interéses

--- Intereses/Intereses.java
+++ Intereses/Intereses.java
@@ -2,11 +2,33 @@
 public class Intereses
 {
   public static void main(String[] args) {
-    double monto;
-    double principal = 100.0;
-    double tasa = 0.05;
+        double monto;
+        double principal = 1000.0;
+        double tasa;
 
-    //Encabezados
-    System.out.printf()
+        //Imprimo encabezados
+        System.out.printf("%s", "Anio");
+
+
+        //Ciclo for para imprimir encabezados de interés del 5% al 10%
+        for (int i = 5; i <= 10; i++)
+        {
+            System.out.printf("%25s %d%s","Monto en deposito", i,"%");
+        }
+        System.out.println();
+
+        //ciclo for para imprimir cada linea iniciando con la bandera del año
+        for (int anio = 1; anio <= 10; anio++)
+        {
+            System.out.printf("%4d", anio);
+
+            //Calculo del monto
+            for (tasa = 0.05; tasa <= 0.1; tasa = tasa + .01)
+            {
+                monto = principal * Math.pow(1.0 + tasa, anio);
+                System.out.printf("%,28.2f", monto);
+            }
+            System.out.println();
+        }
   }
 }
