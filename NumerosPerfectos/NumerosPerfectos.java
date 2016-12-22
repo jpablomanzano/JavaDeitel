c431e1cfc9212ca340ae1cea6c5f8503dd75f256
jpablomanzano <juanpi9_@hotmail.com>
2016-12-22 01:13:27 -0600

    update NumerosPerfectos

--- NumerosPerfectos/NumerosPerfectos.java
+++ NumerosPerfectos/NumerosPerfectos.java
@@ -1,21 +1,56 @@
-import java.util.Scanner;
+//Programa que imprime los números perfectos de 1 al 1,000
+
 
 public class NumerosPerfectos
 {
   public static void main(String[] args)
   {
-    int numero;
+    int numero = 1;
 
-    Scanner input = new Scanner(System.in);
 
-    System.out.print("Ingrese su número: ");
-    numero = input.nextInt();
+    while(numero <= 1000)
+    {
+      if (esPerfecto(numero) == true)
+      {
+        System.out.printf("Número: %d ", numero);
+        System.out.printf("Factoriales: ");
+        for (int i = 1; i < numero; i++)
+        {
+          if (numero % i == 0)
+          {
+            System.out.printf("%d ", i);
+          }
+        }
+        System.out.println();
+      }
+      numero++;
+    }
 
   }
 
-  public static int esPerfecto(int num)
+  public static boolean esPerfecto(int numero)
   {
-    
+    boolean perfecto;
+    int suma = 0;
+
+    for (int i = 1; i < numero; i++)
+    {
+      if (numero % i == 0)
+      {
+          suma = suma + i;
+      }
+    }
+
+    if (suma == numero)
+    {
+        perfecto = true;
+    }
+    else
+    {
+        perfecto = false;
+    }
+
+    return perfecto;
   }
 
 }
