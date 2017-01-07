c9942b253a8c9c7bae5637c8422ef617e04aa599
jpablomanzano <juanpi9_@hotmail.com>
2017-01-07 13:46:54 -0600

    Update AsistenciaPorComputadora

--- AsistenciaPorComputadora/AsistenciaPorComputadora.java
+++ AsistenciaPorComputadora/AsistenciaPorComputadora.java
@@ -9,7 +9,6 @@ public class AsistenciaPorComputadora
 
     //Creo mi objeto random
     private static final Random numerosAleatorios = new Random();
-    private enum Estado {CONTINUA, TERMINA} //enum para evaluar si se continua la prueba o no
 
     //Inicio main
     public static void main(String[] args)
@@ -19,46 +18,42 @@ public class AsistenciaPorComputadora
 
         int numeroAEvaluar; // guarda el la respuesta correcta
         int respuesta; //guarda la respuesta del usuario
-        Estado miEstado = Estado.CONTINUA; // guarda el estado de continuidad del test
+        int contadorPreguntas = 1;
+        int contadorCorrectas = 0;
+        int contadorIncorrectas = 0;
 
+        System.out.print("Bienvenido al asistente de la multiplicacion, presione 1 para continuar y 2 para salir: ");
+        respuesta = input.nextInt();
 
-        System.out.println("Bienvenido al asistente de la multiplicación. Presione -1 para salir.");
-        
-        //Ciclo termina hasta que el usuario presione -1
-        while (miEstado == Estado.CONTINUA)
+        if (respuesta == 1)
         {
+          while (contadorPreguntas <= 10)
+          {
+              numeroAEvaluar = preguntar();
+              respuesta = input.nextInt();
 
-            numeroAEvaluar = preguntar(); //Ejecuto metodo preguntar y el valor de retorno lo guardo en numeroAEvaluar
-            respuesta = input.nextInt(); // Asigno la respuesta del usuario
+              if (respuesta == numeroAEvaluar)
+              {
+                  respuestaCorrecta();
+                  contadorCorrectas++;
+              }
+              else
+              {
+                respuestaIncorrecta();
+                contadorIncorrectas++;
+              }
 
-            //Evaluo si es -1 y termino el programa
-            if (respuesta == -1)
-            {
-                miEstado = Estado.TERMINA; //Cambio el estado a termina
-                System.out.println("Hasta luego"); //Despido el programa
-            }
-            //Si no es -1 entonces continua el programa
-            else
-            {
-                //Evaluo si la respuesta es correcta
-                if (respuesta == numeroAEvaluar)
-                {
-                    respuestaCorrecta(); // Ejecuto el método respuestaCorrecta
-                }
-                else  //Si la respuesta no es correcta
-                {
-                    //Mientras la respuesta sea incorrecta se sigue preguntando
-                    while (respuesta != numeroAEvaluar)
-                    {
-                        respuestaIncorrecta(); //Ejecuto metodo respuestaIncorrecta
-                        respuesta = input.nextInt(); // Asigno la respuesta a variable
-                    }
-                    respuestaCorrecta(); //Ejecuto metodo respuestaCorrecta
-                }
+              contadorPreguntas++;
+          }
+          System.out.println(contadorCorrectas);
+          System.out.println(contadorIncorrectas);
 
-                miEstado = Estado.CONTINUA;
-            }
         }
+        else
+        {
+          System.out.println("Hasta luego!");
+        }
+
     } //fin main
 
 
